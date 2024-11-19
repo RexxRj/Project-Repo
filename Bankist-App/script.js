2c2bc31a4436d3a8d527b670b91207a3a402f127
RexxRj <103634570+RexxRj@users.noreply.github.com>
2024-11-19 17:37:28 +0530

    Request Loan Implemented

--- Bankist-App/script.js
+++ Bankist-App/script.js
@@ -171,6 +171,21 @@ btnTransfer.addEventListener('click', function(e){
   }
 });
 
+btnLoan.addEventListener('click', function(e){
+  e.preventDefault();
+
+  const amount = Number(inputLoanAmount.value);
+
+  if(amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1))
+  {
+    currentAccount.movements.push(amount);
+    UpdateUI(currentAccount);
+  }
+
+  inputLoanAmount.value = '';
+  inputLoanAmount.blur();
+})
+
 btnClose.addEventListener('click', function(e){
   e.preventDefault();
 
