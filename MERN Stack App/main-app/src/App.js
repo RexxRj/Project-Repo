47c05f99e7c46bd53ef2ae5be8ba9eef8347b8b4
RexxRj <103634570+RexxRj@users.noreply.github.com>
2024-12-14 20:23:05 +0530

    Added Modal scripts

--- MERN Stack App/main-app/src/App.js
+++ MERN Stack App/main-app/src/App.js
@@ -7,6 +7,7 @@ import {
 import Users from "./user/pages/Users";
 import NewPlace from "./places/pages/NewPlaces";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
+import UserPlaces from "./places/pages/UserPlaces";
 import "./App.css";
 
 const App = () => {
@@ -17,6 +18,9 @@ const App = () => {
         <Switch>
           <Route path="/" exact>
             <Users />
+          </Route>
+          <Route path="/:userId/places" exact>
+            <UserPlaces />
           </Route>
           <Route path="/places/new" exact>
             <NewPlace />
