cfd35da59937d18378ef4d825cb04ed56e95ca92
RexxRj <103634570+RexxRj@users.noreply.github.com>
2024-12-27 17:32:23 +0530

    Blog app frontend done!

--- my_site/blog/views.py
+++ my_site/blog/views.py
@@ -1,12 +1,61 @@
 from django.shortcuts import render
+from datetime import date
 
-# Create your views here.
+all_posts = [
+    {
+        "slug": "hike-in-the-mountains",
+        "image": "mountains.jpg",
+        "author": "Rexx",
+        "date": date(2021, 7, 21),
+        "title": "Mountain Hiking",
+        "excerpt": "There's nothing like the views you get when hiking in the mountains!",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        """
+    },
+    {
+        "slug": "into-the-woods",
+        "image": "woods.jpg",
+        "author": "Barsha",
+        "date": date(2022, 7, 22),
+        "title": "Into the Woods",
+        "excerpt": "Get lost in the woods with me and enjoy the sounds of nature.",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        """
+    },
+    {
+        "slug": "coding-is-fun",
+        "image": "coding.jpg",
+        "author": "Rexx",
+        "date": date(2024, 8, 16),
+        "title": "Programming is fun",
+        "excerpt": "Programming is fun and challenging. It's a great way to exercise your brain.",
+        "content": """
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Quibusdam debitis adipisci quae fugit earum doloribus ipsum corporis est, distinctio asperiores, blanditiis eius illum eos tenetur vel corrupti nihil quaerat aperiam.
+        """
+    }
+]
+
+sorted_posts = sorted(all_posts, key=lambda post: post['date'], reverse=True)
+latest_posts = sorted_posts[:3]
 
 def index(request):
-    return render(request, 'blog/index.html')
+    return render(request, 'blog/index.html',{
+        "posts": latest_posts
+    })
 
 def posts(request):
-    return render(request, 'blog/posts.html')
+    return render(request, 'blog/all-posts.html',{
+        "posts": sorted_posts
+    })
 
 def post_details(request, slug):
-    return render(request, 'blog/post_details.html', {'slug': slug})
+    post_detail = next(post for post in all_posts if post['slug'] == slug)
+    return render(request, 'blog/post-details.html', {'post_detail': post_detail})
