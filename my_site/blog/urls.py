24e6dad6a7c318ee6060d6cb6b3ff3694b027768
RexxRj <103634570+RexxRj@users.noreply.github.com>
2025-01-19 14:55:10 +0530

    Blog project build complete 🎉

--- my_site/blog/urls.py
+++ my_site/blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.StartingPageView.as_view(), name='index-page'),
     path('posts', views.AllPostsView.as_view(), name='posts-page'),
     path('posts/<slug:slug>', views.PostDetailsView.as_view(), name='post-details-page'),
+    path('read-later',views.ReadLaterView.as_view(),name='read-later-page')
 ]
