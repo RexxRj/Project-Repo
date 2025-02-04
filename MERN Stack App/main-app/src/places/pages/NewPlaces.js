c0cba11f0c8c5cf9a95788736792393cc38f1b95
RexxRj <103634570+RexxRj@users.noreply.github.com>
2025-02-04 23:10:54 +0530

    added validators

--- MERN Stack App/main-app/src/places/pages/NewPlaces.js
+++ MERN Stack App/main-app/src/places/pages/NewPlaces.js
@@ -1,17 +1,34 @@
-import React from "react";
+import React, { useCallback } from "react";
 
 import Input from "../../shared/components/FormElements/Input";
 import "./NewPlace.css";
+import {
+  VALIDATOR_MINLENGTH,
+  VALIDATOR_REQUIRE,
+} from "../../shared/util/validators";
 
 const NewPlace = () => {
+  const titleInputHandler = useCallback((id, value, isValid) => {}, []);
+  const descriptionInputHandler = useCallback((id, value, isValid) => {}, []);
+
   return (
     <form className="place-form">
       <Input
+        id="title"
         element="input"
         type="text"
         label="Title"
-        validators={[]}
+        validators={[VALIDATOR_REQUIRE()]}
         errorText="Please enter a valid Title!"
+        onInput={titleInputHandler}
+      />
+      <Input
+        id="description"
+        element="textarea"
+        label="Description"
+        validators={[VALIDATOR_MINLENGTH(5)]}
+        errorText="Please enter a valid Description (at least 5 characters)!"
+        onInput={descriptionInputHandler}
       />
     </form>
   );
